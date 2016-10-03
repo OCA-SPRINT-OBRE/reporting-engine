5276e1718b387c466931cfba7ccc56c1f3e5cd7a
jmabe <jm.abe@labruyere.be>
2016-10-03 14:22:59 +0200

    [add] web dynamic reporting - creation first commit (empty snippet)

---
+++ web_dynamic_reporting/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    # Theme information
+    'name': "Snippet flexslider ",
+    'description': """
+    """,
+    'version': '1.0',
+    'depends': ['website'],
+
+    # templates, pages, and snippets
+    'data': [
+        'views/snippets.xml',
+   ],
+    'demo': [],
+    'qweb': [],
+    # Your information
+    'installable':True,
+    'author': "Jean-Michel Abé",
+    'website': "",
+}
